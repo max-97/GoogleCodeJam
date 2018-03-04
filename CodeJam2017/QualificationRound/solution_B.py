69737a157fc94c6aa5f0c52bd7a97c644b2aaad3
Max Braun <maxbraun97@web.de>
2018-03-04 21:44:33 +0100

    Final for 2017 task B

--- CodeJam2017/QualificationRound/solution_B.py
+++ CodeJam2017/QualificationRound/solution_B.py
@@ -1,40 +1,30 @@
-def create_tidy_number(number_list, index):
-    i = sameNumber[1]
-    if sameNumber[0] != 1:
-        while i <= index:
-            # if number_list[i] == 9:
-            #    break
-            number_list[i] = number_list[i] - 1
-            i = i + 1
-    else:
-        number_list[sameNumber[1]] = 0
-        index = i
-    number_list[index + 1:] = [9] * (len(number_list) - index - 1)
-
-
-fileName = "B-small-practice"
-read = open("problems/" + fileName + ".in", "r")
-write = open("solutions/" + fileName + ".txt", "w")
+fileName = "B-large-practice.in"
+read = open("problems/" + fileName, "r")
+write = open("solutions/" + fileName, "w")
 size = int(read.readline())
 # print(size)
 
-for j in range(size):
+for k in range(size):
     number = read.readline().replace("\n", "")
     numberAsList = [int(x) for x in number]
-    sameNumber = [numberAsList[0], 0]
-    for i in range(len(numberAsList) - 1):
-        if sameNumber[0] != numberAsList[i]:
-            sameNumber = [numberAsList[i], i]
-        if numberAsList[i] > numberAsList[i+1]:
-            create_tidy_number(numberAsList, i)
-            break
-    # print(numberAsList)
+
+    i = len(numberAsList) - 1
+    while i > 0:
+        if numberAsList[i] >= numberAsList[i - 1]:
+            i = i - 1
+            continue
+        numberAsList[i - 1] = numberAsList[i - 1] - 1
+        j = i
+        while j < len(numberAsList):
+            numberAsList[j] = 9
+            j = j + 1
+        i = i - 1
     string = ""
     for x in numberAsList:
         string += str(x)
     result = int(string)
     print(result)
-    write.write("Case #" + str(j+1) + ": " + str(result) + "\n")
+    write.write("Case #" + str(k+1) + ": " + str(result) + "\n")
 read.close()
 write.close()
 
